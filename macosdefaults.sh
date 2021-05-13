acdf15695a17778bcdd096ede984531705bc3a81
hardian.l <hardian.l@gojek.com>
2021-05-13 19:28:10 +0800

    add macosdefaults

---
+++ macosdefaults.sh
@@ -0,0 +1,11 @@
+# Save screenshots to the Downloads
+defaults write com.apple.screencapture location -string "$HOME/Downloads"
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=” “
