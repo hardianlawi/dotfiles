3c2bb3ae751a82c7873f47f323bc89b94a6a0f65
hardianlawi <hardianlawi@gmail.com>
2021-06-08 23:26:50 +0800

    update linux_setup

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
+setup_environment () {
+    sudo apt update
+    sudo apt install -y zsh rake wget git autojump
+}
+
 install_miniconda () {
     if ! command -v conda &> /dev/null
     then
@@ -12,9 +17,25 @@ install_miniconda () {
     fi
 }
 
+update_zshrc () {
+
+    sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="spaceship"/g' $HOME/.zshrc
+    sed -i 's/plugins=(git)/plugins=(autojump git history z zsh-autosuggestions kubectl)/g' $HOME/.zshrc
+
+    echo "" >> $HOME/.zshrc
+    echo "\ncode () { VSCODE_CWD="$PWD" open -n -b "com.microsoft.VSCode" --args $* ;}\n" >> $HOME/.zshrc
+    echo "" >> $HOME/.zshrc
+
+}
+
 customize_terminal () {
 
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    update_zshrc
+
+    source $HOME/.zshrc
+
     git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
     ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -23,5 +44,6 @@ customize_terminal () {
     curl -L https://bit.ly/janus-bootstrap | bash
 }
 
+setup_environment
 install_miniconda
 customize_terminal
