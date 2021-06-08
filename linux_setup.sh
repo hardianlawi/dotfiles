d759242f04a9345426943c8704bba385dbec5b3c
hardianlawi <hardianlawi@gmail.com>
2021-06-08 18:08:00 +0800

    refactor linux_setup

--- linux_setup.sh
+++ linux_setup.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 set -ex
 
-wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-x86_64.sh
+install_miniconda () {
+    if ! command -v conda &> /dev/null
+    then
+        echo "Installing miniconda..."
+        wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-MacOSX-arm64.sh
+        bash Miniforge3-MacOSX-arm64.sh
+        rm Miniforge3-MacOSX-arm64.sh
+        echo "Please run `conda config --set changeps1 True` to turn off the conda environment display"
+    fi
+}
 
-bash Miniforge3-Linux-x86_64.sh
-rm Miniforge3-Linux-x86_64.sh
+customize_terminal () {
 
-curl -L https://bit.ly/janus-bootstrap | bash
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # https://github.com/carlhuda/janus
+    curl -L https://bit.ly/janus-bootstrap | bash
+}
 
-git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
-ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
-
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+install_miniconda
+customize_terminal
