74f4b92509a65cb1e9028caea78baee0e8e9cce4
hardianlawi <hardianlawi@gmail.com>
2021-06-14 12:08:25 +0800

    add lts nodejs installation

--- ubuntu.sh
+++ ubuntu.sh
@@ -4,9 +4,15 @@ set -ex
 . ./setup.sh
 
 setup_environment () {
+    # Latest NodeJs
+    # https://github.com/nodesource/distributions
+    curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+
     sudo apt update
     sudo apt install -y zsh rake wget git autojump \
-        make gcc build-essential tmux g++ htop
+        make gcc build-essential tmux g++ htop \
+        nodejs npm
+
     curl https://get.docker.com | bash
 }
 
