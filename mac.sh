c72fde506208d8ed59665a1cab0ed24ef4140b5b
Hardian Lawi <hardianlawi@gmail.com>
2024-04-15 23:19:49 +0800

    Update mac.sh

--- mac.sh
+++ mac.sh
@@ -5,6 +5,8 @@ set -ex
 
 setup_brew () {
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/hardianlawi/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
     brew tap homebrew/cask
     brew tap homebrew/cask-versions
 }
